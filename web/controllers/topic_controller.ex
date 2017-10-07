e65382507116029cf04f3f4bec389a30c4f0b225
Connor O'Brien <cobrien.js@gmail.com>
2017-10-06 21:32:38 -0500

    Finishing Auth, Starting Websockets

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -10,14 +10,19 @@ defmodule Discuss.TopicController do
   plug Discuss.Plugs.RequireAuth when action in [
     :new, :create, :edit, :update, :delete
   ]
-  # plug :check_topic_owner when action in [
-    # :update, :edit, :delete
-  # ]
+  plug :check_topic_owner when action in [
+    :update, :edit, :delete
+  ]
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
+  end
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render(conn, "show.html", topic: topic)
   end
 
   def new(conn, _params) do
@@ -43,7 +48,7 @@ defmodule Discuss.TopicController do
   end
 
   def edit(conn, %{"id" => topic_id}) do
-    topic = Repo.get(Topic, topic_id)
+    topic = Repo.get!(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
     render conn, "edit.html", changeset: changeset, topic: topic
@@ -74,6 +79,16 @@ defmodule Discuss.TopicController do
     |> redirect(to: topic_path(conn, :index))
   end
 
-  # def check_topic_owner do
-  # end
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
+  end
 end
